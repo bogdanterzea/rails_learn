6bdafb212e0a7aadac913a5d4d9802df22735137
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2020-04-15 16:41:05 +0300

    Add goodbye action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-
     def hello
-      render html: "hello, world!"
+     render html: "¡Hola, mundo!"
+    end
+
+    def goodbye
+     render html: "Goodbye sir!"
     end
 end
